fb54d539bdd6fc98479667e8efe4d05422cdb879
HanJunhee <hanjuni857@scg.skku.ac.kr>
2022-06-26 19:42:49 +0900

    수정

--- back/src/game/game.service.ts
+++ back/src/game/game.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Field } from './entities/field.entity';
 import { Game } from './entities/game.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class GameService {
-  constructor(@InjectRepository(Game) private gameRepositary: Repository<Game>){}
+  constructor(@InjectRepository(Game) private gameRepository: Repository<Game>){}
   //constructor(@InjectRepository(Field) private fieldRepositary: Repository<Field>){}
   
-  async findAllGames(){
-    return await this.gameRepositary.find(); //모든 게임 목록 반환
+  async findAllGames():Promise<Game[]>{
+    return await this.gameRepository.find();
   }
 }
