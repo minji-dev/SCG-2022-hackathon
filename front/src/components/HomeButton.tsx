2cac69fe02009a65a4d98275b3663ad60c38ba8d
BestSanghyeon <qazxsw100415@gmail.com>
2022-06-26 21:35:56 +0900

    <EDIT> Home Button Design

--- front/src/components/HomeButton.tsx
+++ front/src/components/HomeButton.tsx
@@ -10,6 +10,21 @@ function HomeButton() {
                     홈으로
                 </button>
             </Link>
+            <style jsx>{`
+                @import url(https://cdn.jsdelivr.net/gh/moonspam/NanumSquare@1.0/nanumsquare.css);
+
+                button{
+                    width: 180px;
+                    height: 50px;
+                    background: #333D79;
+                    border-radius: 20px;
+                    border: 0;
+                    color: #ffffff;
+                    font-family: 'NanumSquare', sans-serif;
+                    font-size: 22px;
+                    font-weight: 500;
+                }
+                `}</style>
         </div>
         )
 }
