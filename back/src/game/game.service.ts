6720230f4aded60305a5fb242b4562409a03bc02
HanJunhee <hanjuni857@scg.skku.ac.kr>
2022-06-28 00:45:32 +0900

    원하는 것만 조회하도록 수정

--- back/src/game/game.service.ts
+++ back/src/game/game.service.ts
@@ -17,6 +17,19 @@ export class GameService {
     .leftJoinAndSelect('g.game_file', 'g_fi')  //게임 - 게임파일 join
     .leftJoinAndSelect('g_fi.file_id', 'fi') //게임파일 - 첨부파일 join
     //.leftJoinAndSelect('fi.field_file', 'f_fi') //첨부파일 - 분야파일 join
+    .select([
+      'g.id',
+      'g.name',
+      'g.context',
+      'g.level',
+      'f.id',
+      'f.name',
+      'g_fi.type',
+      'fi.id',
+      'fi.name',
+      'fi.size',
+      'fi.mime'
+    ])
     .where('f.name = :category', {category: category})
     .getMany(); 
   }
