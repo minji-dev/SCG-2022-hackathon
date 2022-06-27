9d8ad4cd98f69c48c7acb94573a35f6799fde1d5
mj.park <gbsmj0312@gmail.com>
2022-06-27 17:15:48 +0900

    need querybuilder test

--- back/src/helpers/helpers.service.ts
+++ back/src/helpers/helpers.service.ts
@@ -1,38 +1,108 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Db, Repository } from 'typeorm';
 import { Game } from '../entities/game.entity';
-import { Repository } from 'typeorm';
-import { Field } from '../entities/field.entity';
-import { File } from '../entities/file.entity';
+import { Game_File } from "../entities/game_file.entity";
+import { Field_File } from "../entities/field_file.entity";
+
 
 @Injectable()
 export class HelpersService {
   constructor(
     @InjectRepository(Game) private gameRepository: Repository<Game>,
-    // @InjectRepository(Field) private fieldRepository: Repository<Field>,
-    // @InjectRepository(file) private fileRepository: Repository<file>,
+    // @InjectRepository(Game_File) private fieldRepository: Repository<Game_File>,
+    // @InjectRepository(Field_File) private fileRepository: Repository<Field_File>,
     ){}
     
-  async getAllHelpers(): Promise<Game[]>{ //전체 헬퍼. 3개. 굳이?
+  async getAllHelpers() { //전체 헬퍼. 3개. 굳이?
+    const result = [];
     const helpers: string[] = ["훈민정음", "레코드EEE", "지하철"];
-    return await this.gameRepository.find({
-      where: [
-        { name: helpers[0] }, { name: helpers[1] }, { name: helpers[2] },
-      ]
-    });
+
+    const result_game = await this.gameRepository.createQueryBuilder('g')
+        .innerJoin('g.field', 'f')
+        .innerJoin('g.game_file', 'gf')
+        .innerJoin('gf.file_id', 'fi')
+        .select([
+            'g.name',
+            'g.context',
+            'g.level',
+            'f.name',
+            'gf.type',
+            'fi.name',
+            'fi.size',
+            'fi.mime',
+        ])
+        .where("g.name = :name", { name: helpers[0] })
+        .andWhere("g.name = :name", { name: helpers[1] })
+        .andWhere("g.name = :name", { name: helpers[2] })
+        .getMany();
+
+    result.push(result_game);
+    return result;
 
   }
 
-  async getHelper(): Promise<Game>{ //헬퍼 중 하나
+  async getHelper() { //헬퍼 중 하나
+    const result = [];
     const helpers: string[] = ["훈민정음", "레코드EEE", "지하철"];
     let random = Math.floor(Math.random() * 3);
-    let random_helper = helpers[random];
-    return await this.gameRepository.findOneBy({ name: random_helper });
+    let index = helpers[random];
+
+    const result_game = await this.gameRepository.createQueryBuilder('g')
+        .innerJoin('g.field', 'f')
+        .select([
+            'g.name',
+            'g.context',
+            'g.level',
+            'f.name',
+        ])
+        .where("g.name = :name", { name: index })
+        .getMany();
+
+    const result_file = await this.gameRepository.createQueryBuilder('g')
+        .innerJoin('g.game_file', 'gf')
+        .innerJoin('gf.file_id', 'f')
+        .select([
+            'gf.type',
+            'f.name',
+            'f.size',
+            'f.mime',
+        ])
+        .where("g.name = :name", { name: index })
+        .getMany();
+
+    result.push(result_game, result_file);
+    return result;
   }
 
-  async getRandom(): Promise<Game>{
-    let random = Math.floor(Math.random() * 6 + 1);
-    // let random = Math.floor(Math.random() * 19 + 1);
-    return await this.gameRepository.findOneBy({ id: random });
+  async getRandom() {
+    const result = [];
+    let random = Math.floor(Math.random() * 19 + 1);
+
+    const result_game = await this.gameRepository.createQueryBuilder('g')
+        .innerJoin('g.field', 'f')
+        .select([
+            'g.name',
+            'g.context',
+            'g.level',
+            'f.name',
+        ])
+        .where("g.id = :id", { id: random })
+        .getMany();
+
+    const result_file = await this.gameRepository.createQueryBuilder('g')
+        .innerJoin('g.game_file', 'gf')
+        .innerJoin('gf.file_id', 'f')
+        .select([
+            'gf.type',
+            'f.name',
+            'f.size',
+            'f.mime',
+        ])
+        .where("g.id = :id", { id: random })
+        .getMany();
+
+    result.push(result_game, result_file);
+    return result;
   }
 }
