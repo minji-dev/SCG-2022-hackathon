c548bc2e77f6ad619d6e4bfbb6a5a99db13b9540
HanJunhee <hanjuni857@scg.skku.ac.kr>
2022-06-28 21:29:01 +0900

    딱히 수정한 거 없음

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -10,7 +10,7 @@ import { HelpersModule } from './helpers/helpers.module';
     host: 'localhost',
     port: 3306,
     username: 'root',
-    password: 'alswl0312',
+    password: '1234',
     database: 'scg',
     autoLoadEntities: true,
     synchronize: true,
