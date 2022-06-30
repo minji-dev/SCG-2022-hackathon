f49de04226b261109d53b4aed6a9a599ce457b0e
Yi <hoon_5083@naver.com>
2022-07-01 00:00:04 +0900

    <fix>

--- front/src/components/RandomButton.tsx
+++ front/src/components/RandomButton.tsx
@@ -7,11 +7,7 @@ function RandomButton() {
   return (
     <div className="flex justify-center">
       <Link href={`/randomgames`}>
-<<<<<<< HEAD
         <span className="cursor-pointer hover:bg-slate-500 hover:ring-4 ring-offset-4 ring-slate-500 transition m-auto p-2 w-[200px] h-12 bg-[#333d79] rounded-2xl text-white text-2xl font-medium text-center">랜덤~~~~게임</span>
-=======
-        <span className="cursor-pointer hover:bg-slate-500 hover:ring-4 ring-offset-4 ring-slate-500 transition m-auto p-2 w-44 h-12 bg-[#333d79] rounded-2xl text-white text-2xl font-medium text-center">랜덤~~게임</span>
->>>>>>> 2e89ff6d64483b84d6e1ad51759a8b30c73d52d4
       </Link>
     </div>
   );
