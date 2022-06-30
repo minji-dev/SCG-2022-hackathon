53b487427975f9a91fcf089e7c01321b95ea65fb
BestSanghyeon <qazxsw100415@gmail.com>
2022-06-30 21:17:26 +0900

    인트로 듣기 버튼 컴포넌트에 필요 없는 코드 제거

--- front/src/components/ExampleButton.tsx
+++ front/src/components/ExampleButton.tsx
@@ -12,7 +12,6 @@ function ExampleButton({ id }: Props) {
 
   return (
     <div className="flex justify-center">
-        <audio id='audio_play' src='./사운드 파일.mp3'></audio>
         <span onClick={playIntro} className="cursor-pointer hover:bg-slate-500 hover:ring-4 ring-offset-4 ring-slate-500 transition h-12 w-auto border-2 bg-[#333d79] justify-center m-auto rounded-2xl px-10 py-1 text-white text-2xl font-medium">
           인트로 듣기
         </span>
