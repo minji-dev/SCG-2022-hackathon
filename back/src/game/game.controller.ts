6a52ceaa2d61c4ecf5d4447daf65f59e04eb9f6a
minji <mj.park@g.skku.edu>
2022-06-30 20:39:11 +0900

    edit

--- back/src/game/game.controller.ts
+++ back/src/game/game.controller.ts
@@ -18,7 +18,7 @@ export class GameController {
     return this.gameService.findGameById(id);
   }
 
-  @Patch('/:id')
+  @Patch('/edit/:id')
   async updateGame(@Param('id') id: number, @Body() game: UpdateGameDto){
     await this.gameService.updateGame(id, game);
     return Object.assign({
