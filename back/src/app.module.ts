2971810ab773c6c76aa87c269e2f5a03c4e7d62b
minji <mj.park@g.skku.edu>
2022-06-30 21:44:40 +0900

    .env 도입

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,26 +1,32 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config'
+
 import { GameModule } from './game/game.module';
 import { HelpersModule } from './helpers/helpers.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { File } from './entities/file.entity';
 import { Field } from './entities/field.entity';
 import { Field_File } from "./entities/field_file.entity";
 
+
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'minji0312!',
-    database: 'scg',
-    autoLoadEntities: true,
-    synchronize: true,
-    logging: true,
-  }), TypeOrmModule.forFeature([File, Field, Field_File]),GameModule, HelpersModule],
+      type: 'mysql',
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      database: process.env.DB_SCHEMA,
+      entities: [],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([File, Field, Field_File]),
+    GameModule, HelpersModule],
   controllers: [AppController],
   providers: [AppService],
 })
