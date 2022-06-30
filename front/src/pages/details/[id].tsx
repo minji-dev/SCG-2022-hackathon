431756b95da9981c6f6dbb25fade035d4abe3b4a
BestSanghyeon <qazxsw100415@gmail.com>
2022-06-30 22:02:22 +0900

    술게임 설명 간격 수정

--- front/src/pages/details/[id].tsx
+++ front/src/pages/details/[id].tsx
@@ -14,7 +14,7 @@ const Detail: NextPage = () => {
   console.log(games);
 
   return (
-    <div className="text-[#333d79]">
+    <div className="text-[#333d79] block">
       <PageHead title={games?.at(0)?.name} />
       <h1 className="justify-center w-auto m-2 text-5xl font-bold text-center">
         {games?.at(0)?.name}
@@ -29,17 +29,19 @@ const Detail: NextPage = () => {
           </p>
         </div>
         <ExampleButton id={Number(id)} />
-        <div className="justify-center w-[1181px] m-auto">
+        <div className="justify-center w-[1181px] mt-10 m-auto">
           <p className="text-left border-b-[3px] p-2 border-[#333d79] font-bold text-3xl">
             규칙
           </p>
           <p className="text-xl font-[550] p-2 text-left">{games?.at(0)?.context}</p>
-          <BackToCatButton id={games?.at(0)?.field.id}/>
           <Link href={`/details/edit/${id}`}>
-            <span className="cursor-pointer hover:bg-slate-500 hover:ring-4 ring-offset-4 ring-slate-500 transition h-12 w-auto border-2 bg-[#333d79] justify- m-auto rounded-3xl px-6 py-2 text-white text-xl inline-block float-right font-medium">
-              수정
-            </span>
+            <div className="block flex float-right">
+              <span className="cursor-pointer hover:bg-slate-500 hover:ring-4 ring-offset-4 ring-slate-500 transition h-12 w-auto border-2 bg-[#333d79] rounded-3xl px-6 py-2 text-white text-xl inline-block float-right font-medium">
+                수정
+              </span>
+            </div>
           </Link>
+          <BackToCatButton id={games?.at(0)?.field.id}/>
         </div>
       </div>
     </div>
